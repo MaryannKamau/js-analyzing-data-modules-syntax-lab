fc778fb09d53b0e1250d3eac6542cee42162d66a
Maryann Kamau <5806maryannmuthoni@gmail.com>
2026-01-11 21:12:24 +0300

    used Javascript

--- index.js
+++ index.js
@@ -1,5 +1,24 @@
 
+require('datejs'); 
 
+
+function combineUsers(...args) {
+    
+    let combinedObject = {
+        users: []
+    };
+
+    
+    for (let i = 0; i < args.length; i++) {
+        let currentArray = args[i];
+
+        combinedObject.users = [...combinedObject.users, ...currentArray];
+    }
+
+
+    combinedObject.merge_date = Date.today().toString("M/d/yyyy");
+    return combinedObject;
+}
 
 module.exports = {
   ...(typeof combineUsers !== 'undefined' && { combineUsers })
